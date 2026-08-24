55292baf8669bcb996247d870914dd0096f15447
sooji7435 <bys0634@naver.com>
2026-08-24 15:10:20 +0900

    chore: Xcode 프로젝트 초기 생성

---
+++ Pickple/Pickple/PickpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickpleApp.swift
+//  Pickple
+//
+//  Created by 박윤수 on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct PickpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pickple/PickpleUITests/PickpleUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  PickpleUITestsLaunchTests.swift
+//  PickpleUITests
+//
+//  Created by 박윤수 on 8/24/26.
+//
+
+import XCTest
+
+final class PickpleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
