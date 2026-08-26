d5c6c12dd599d2b3958ac7c94f566f95ce3e59e3
sooji7435 <bys0634@naver.com>
2026-08-26 13:34:44 +0900

    feat: 로그인 화면 UI 구현

---
+++ Pickple/Pickple/Presentation/Login/LoginStrings.swift
@@ -0,0 +1,13 @@
+//
+//  LoginString.swift
+//  Pickple
+//
+//  Created by 박윤수 on 8/26/26.
+//
+
+enum LoginStrings {
+    static let kakaoLogin = "카카오로 로그인"
+    static let appleLogin = "Apple로 로그인"
+    static let guestLogin = "게스트로 둘러보기"
+    static let termsNotice = "가입을 진행하시면 서비스 이용 약관 및\n 개인정보처리방침에 동의하시는 것으로 간주됩니다."
+}
---
+++ Pickple/Pickple/Presentation/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Pickple
+//
+//  Created by 박윤수 on 8/24/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
