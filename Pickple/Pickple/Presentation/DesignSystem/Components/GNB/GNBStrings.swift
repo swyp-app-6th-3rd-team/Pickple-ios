7f5d62fd579c35f519825bb2e03b89cd67e377f7
sooji7435 <bys0634@naver.com>
2026-08-27 12:23:51 +0900

    feat: PickpleGNB 타입별 레이아웃 구현

---
+++ Pickple/Pickple/Presentation/DesignSystem/Components/GNB/GNBStrings.swift
@@ -0,0 +1,14 @@
+//
+//  PickpleGNBStrings.swift
+//  Pickple
+//
+//  Created by 박윤수 on 8/27/26.
+//
+//  TODO: 디자인 확정 후 실제 문구로 교체
+//
+
+enum GNBStrings {
+    static let searchTitle = "search"
+    static let backTitle = "back"
+    static let alertTitle = "alert"
+}
