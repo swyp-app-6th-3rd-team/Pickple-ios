605de4e1754a7bf145a0109c17669d6801d14e57
sooji7435 <bys0634@naver.com>
2026-08-27 11:18:15 +0900

    feat: PickpleGNB 컴포넌트 추가

---
+++ Pickple/Pickple/Presentation/DesignSystem/Components/GNB/PickpleGNB.swift
@@ -0,0 +1,20 @@
+//
+//  PickpleGNB.swift
+//  Pickple
+//
+//  Created by 박윤수 on 8/27/26.
+//
+
+import SwiftUI
+
+enum PcikpleGNB {
+    case searchGNB
+    case backGNB
+    case alertGNB
+}
+
+struct PickpleGNB: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
