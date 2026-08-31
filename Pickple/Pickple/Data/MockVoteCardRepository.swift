11541ab02dc9c38e1fbe57508a4a107f5011d53b
sooji7435 <bys0634@naver.com>
2026-09-01 01:51:14 +0900

    Mock data 추가 생성

--- Pickple/Pickple/Data/MockVoteCardRepository.swift
+++ Pickple/Pickple/Data/MockVoteCardRepository.swift
@@ -10,6 +10,13 @@ struct MockVoteCardRepository: VoteCardRepository {
     func fetchCards() async -> [VoteCard] {
         [
             VoteCard(id: UUID(), type: .forAgainst, productName: "무선 이어폰", concernText: "이거 살까 말까 고민이에요", imageName: "MockAgainstPicture"),
+            VoteCard(id: UUID(), type: .forAgainst, productName: "무선 이어폰", concernText: "이거 살까 말까 고민이에요", imageName: "MockAgainstPicture"),
+            VoteCard(id: UUID(), type: .forAgainst, productName: "무선 이어폰", concernText: "이거 살까 말까 고민이에요", imageName: "MockAgainstPicture"),
+            VoteCard(id: UUID(), type: .forAgainst, productName: "무선 이어폰", concernText: "이거 살까 말까 고민이에요", imageName: "MockAgainstPicture"),
+            VoteCard(id: UUID(), type: .forAgainst, productName: "무선 이어폰", concernText: "이거 살까 말까 고민이에요", imageName: "MockAgainstPicture"),
+            VoteCard(id: UUID(), type: .forAgainst, productName: "무선 이어폰", concernText: "이거 살까 말까 고민이에요", imageName: "MockAgainstPicture"),
+            VoteCard(id: UUID(), type: .forAgainst, productName: "무선 이어폰", concernText: "이거 살까 말까 고민이에요", imageName: "MockAgainstPicture"),
+            
             //VoteCard(id: UUID(), type: .compare, productName: "노트북 A vs B", concernText: "둘 중 뭐가 나을까요", imageName: "PickpleTitle"),
         ]
     }
