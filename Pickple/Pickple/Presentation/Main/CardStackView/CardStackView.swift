b40e7b342db454da4849f01979f4a3918dc45e20
sooji7435 <bys0634@naver.com>
2026-09-01 01:44:28 +0900

    CardStackView ui 생성

--- Pickple/Pickple/Presentation/Main/CardStackView/CardStackView.swift
+++ Pickple/Pickple/Presentation/Main/CardStackView/CardStackView.swift
@@ -8,13 +8,66 @@
 import SwiftUI
 
 struct CardStackView: View {
+    @StateObject var cardStackViewModel: CardStackViewModel
     var body: some View {
         ZStack {
-            ForEach
+            ForEach(cardStackViewModel.voteCardData) { data in
+                VStack(spacing: 0) {
+                    Image(data.imageName)
+                        .resizable()
+                        .clipShape(UnevenRoundedRectangle(
+                            topLeadingRadius: 16,
+                            topTrailingRadius: 16,
+                            style: .continuous
+                        ))
+                        .frame(width: 333, height: 296)
+                    
+                    ZStack {
+                        UnevenRoundedRectangle(
+                            bottomLeadingRadius: 16,
+                            bottomTrailingRadius: 16,
+                            style: .continuous
+                        )
+                        .foregroundStyle(Color.white)
+                        .shadow(color: Color.black.opacity(0.08), radius: 12, x: 0, y: 0)
+                        
+                        VStack(alignment: .leading, spacing: 12) {
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text(data.productName)
+                                    .pickpleTypography(.title02)
+                                    .foregroundStyle(Color.neutral100)
+                                
+                                Text(data.concernText)
+                                    .pickpleTypography(.body02)
+                                    .foregroundStyle(Color.neutral40)
+                            }
+                            .padding(.horizontal, 20)
+                            HStack(spacing: 8) {
+                                Button(action: {}) {
+                                    Text("사자")
+                                }
+                                .buttonStyle(.pickple(._default, 48))
+                                
+                                Button(action: {}) {
+                                    Text("말자")
+                                }
+                                .buttonStyle(.pickple(._default, 48))
+                                
+                            }
+                            .padding(.horizontal, 20)
+                        }
+                    }
+                    .frame(width: 333, height: 144)
+                }
+            }
+        }
+        .task {
+            await cardStackViewModel.loadCards()
         }
     }
+    
 }
 
 #Preview {
-    PickpleCardStackView()
+    CardStackView(cardStackViewModel: CardStackViewModel())
 }
