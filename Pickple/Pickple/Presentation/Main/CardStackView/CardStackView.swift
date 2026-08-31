1ed186e58346e0e88695ae75af1114202e167e36
sooji7435 <bys0634@naver.com>
2026-09-01 00:19:51 +0900

    feat: 카드스택 Mock 데이터 파이프라인 추가

---
+++ Pickple/Pickple/Presentation/Main/CardStackView/CardStackView.swift
@@ -0,0 +1,20 @@
+//
+//  PickpleCardStackView.swift
+//  Pickple
+//
+//  Created by 박윤수 on 8/31/26.
+//
+
+import SwiftUI
+
+struct CardStackView: View {
+    var body: some View {
+        ZStack {
+            ForEach
+        }
+    }
+}
+
+#Preview {
+    PickpleCardStackView()
+}
