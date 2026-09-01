143452fd9aa7f931f5be86e160c194f0b7469bda
sooji7435 <bys0634@naver.com>
2026-09-01 17:41:13 +0900

    feat/MyPageProfileView 생성 및 UI 구성

--- Pickple/Pickple/Presentation/MyPage/MyPageProfile/MyPageProfileSectionView.swift
+++ Pickple/Pickple/Presentation/MyPage/MyPageProfile/MyPageProfileSectionView.swift
@@ -1,5 +1,5 @@
 //
-//  MyPageProfileImageView.swift
+//  MyPageProfileSectionView.swift
 //  Pickple
 //
 //  Created by 박윤수 on 9/1/26.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct MyPageProfileImageView: View {
+struct MyPageProfileSectionView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MyPageProfileImageView()
+    MyPageProfileSectionView()
 }
