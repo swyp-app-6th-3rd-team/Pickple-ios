b1f7f8a30023c48ec15f5808a2f5408981b25e1f
sooji7435 <bys0634@naver.com>
2026-09-02 15:28:42 +0900

    feat: MyPagePostView 초기 UI 생성

--- Pickple/Pickple/Presentation/MyPage/MyPagePost/MyPagePostView.swift
+++ Pickple/Pickple/Presentation/MyPage/MyPagePost/MyPagePostView.swift
@@ -11,13 +11,13 @@ struct MyPagePostView: View {
     @ObservedObject var myPageViewModel: MyPageViewModel
     
     var body: some View {
-        VStack {
-            
+        VStack(spacing: 12){
             //XMARK: - Title
             HStack {
                 Text("내가 올린 투표")
                     .pickpleTypography(.title01)
                     .foregroundStyle(Color.black)
+                
                 
                 Spacer()
                 
@@ -29,25 +29,107 @@ struct MyPagePostView: View {
                             .resizable()
                             .frame(width: 16, height: 16)
                     }
-                        
+                    
                 }
                 .pickpleTypography(.body02)
                 .foregroundStyle(Color.neutral40)
             }
-            .padding(20)
+            .padding(.horizontal, 20)
             
             //MARK: - Post
-            ScrollView(.horizontal) {
-                ForEach(myPageViewModel.userInfo?, id: \.self) {
-                    
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(myPageViewModel.posts) { post in
+                        Button(action: {}) {
+                            VStack(spacing: 8) {
+                                ZStack (alignment: .topLeading){
+                                    Image(post.imageName)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 160, height: 160) //Fixed
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                    
+                                    HStack {
+                                        switch post.type {
+                                        case .text: Image("PickpleText").resizable().frame(width: 16, height: 16)
+                                        case .forAgainst: Image("PickpleAgainst").resizable().frame(width: 16, height: 16)
+                                        case .compare: Image("PickpleAB").resizable().frame(width: 16, height: 16)
+                                        }
+                                            
+                                        
+                                        
+                                        Text(post.type.displayName)
+                                            .pickpleTypography(.label)
+                                            .foregroundStyle(Color.white)
+                                    }
+                                    .padding(.horizontal, 10)
+                                    .padding(.vertical, 4)
+                                    .background(
+                                        Capsule()
+                                            .foregroundStyle(Color.black)
+                                    )
+                                    .padding(10)
+                                    
+                                }
+                                
+                                
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(post.category)
+                                            .pickpleTypography(.caption)
+                                            .foregroundStyle(Color.neutral50)
+                                        
+                                        Text(post.title)
+                                            .lineLimit(1)
+                                            .pickpleTypography(.body01)
+                                            .foregroundStyle(Color.black)
+                                    }
+                                    Spacer()
+                                }
+                                
+                                
+                                HStack {
+                                    HStack(spacing: 12) {
+                                        HStack(spacing: 4) {
+                                            Image("PickpleVote")
+                                                .resizable()
+                                                .frame(width: 16, height: 16)
+                                            Text("\(post.voteCount)")
+                                        }
+                                        
+                                        HStack(spacing: 4) {
+                                            Image("PickpleComment")
+                                                .resizable()
+                                                .frame(width: 16, height: 16)
+                                            
+                                            Text("\(post.commentCount)")
+                                        }
+                                    }
+                                    .pickpleTypography(.label)
+                                    .foregroundStyle(Color.neutral30)
+                                    
+                                    Spacer()
+                                    
+                                    Text(post.createdAt.relativeTimeDescription)
+                                        .pickpleTypography(.caption)
+                                        .foregroundStyle(Color.neutral40)
+                                }
+                                
+                            }
+                            .frame(width: 160)
+                        }
+                    }
                 }
-                
             }
+            .padding(.horizontal, 20)
             
+        }
+        .task {
+            await myPageViewModel.loadMyPosts()
         }
     }
 }
 
 #Preview {
-    SwiftUIView(myPageViewModel: MyPageViewModel())
+    MyPagePostView(myPageViewModel: MyPageViewModel())
 }
