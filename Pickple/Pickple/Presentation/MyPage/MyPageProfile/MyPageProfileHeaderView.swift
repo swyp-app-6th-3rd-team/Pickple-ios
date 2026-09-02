263928b74c704cb2468feb92150c4b7c4a8be5fd
sooji7435 <bys0634@naver.com>
2026-09-02 19:31:07 +0900

    chore: 코드 정리

--- Pickple/Pickple/Presentation/MyPage/MyPageProfile/MyPageProfileHeaderView.swift
+++ Pickple/Pickple/Presentation/MyPage/MyPageProfile/MyPageProfileHeaderView.swift
@@ -11,27 +11,32 @@ struct MyPageProfileHeaderView: View {
     @ObservedObject var myPageViewModel: MyPageViewModel
 
     var body: some View {
-        VStack(spacing: 12) {
-            Circle()
-                .frame(width: 84, height: 84)
-                .foregroundStyle(Color.white)
-
-            HStack(spacing: 4) {
-                Button(action: {}) {
-                    if let nickname = myPageViewModel.userInfo?.nickname {
-                        Text(nickname)
-                            .pickpleTypography(.title02)
-                            .foregroundStyle(Color.white)
+        Button(action: {}) {
+            VStack(spacing: 12) {
+                Circle()
+                    .frame(width: 84, height: 84)
+                    .foregroundStyle(Color.white)
+                
+                HStack(spacing: 4) {
+                    
+                        if let nickname = myPageViewModel.userInfo?.nickname {
+                            Text(nickname)
+                                .pickpleTypography(.title02)
+                                .foregroundStyle(Color.white)
+                        }
+                        Image("PickpleEdit")
                     }
-                    Image("PickpleEdit")
                 }
             }
-        }
-
+        .padding(.vertical, 30)
         .frame(maxWidth: .infinity)
+        .background(Color.navy60)
+            
+        }
+    
     }
+        
 
-}
 
 #Preview {
     MyPageProfileHeaderView(myPageViewModel: MyPageViewModel())
