c78fd2b0706daf0dcce759700b3611ae37fb2f78
sooji7435 <bys0634@naver.com>
2026-09-02 13:41:18 +0900

    feat: PostSummary 모델 추가

---
+++ Pickple/Pickple/Domain/Entities/PostSummary.swift
@@ -0,0 +1,17 @@
+//
+//  PostSummary.swift
+//  Pickple
+//
+//  Created by 박윤수 on 9/2/26.
+//
+import Foundation
+
+struct PostSummary: Identifiable {
+    let id: UUID
+    let type: VoteType
+    let category: String
+    let title: String
+    let voteCount: Int
+    let commentCount: Int
+    let createdAt: Date
+}
