811f60be579bccb620e9df4154a540f2d270febe
sooji7435 <bys0634@naver.com>
2026-09-02 20:18:20 +0900

    feat: 작성한 게시물이 없을 때 게시물 작성버튼으로 연결되는 MyPagePostCardEmptyView 생성

---
+++ Pickple/Pickple/Presentation/MyPage/MyPagePost/MyPagePostCardEmptyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPagePostCardEmptyView.swift
+//  Pickple
+//
+//  Created by 박윤수 on 9/2/26.
+//
+
+import SwiftUI
+
+struct MyPagePostCardEmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPagePostCardEmptyView()
+}
