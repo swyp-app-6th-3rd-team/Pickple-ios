a2cdb533a279bee4d9e6fa2ee3deec3d6fe22678
sooji7435 <bys0634@naver.com>
2026-09-02 20:32:14 +0900

    fix: GNB 컴포넌트에 text가 아닌 image 폰트 적용되어있던 오류 수정

---
+++ Pickple/Pickple/Presentation/MyActivity/MyActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  MyActivityView.swift
+//  Pickple
+//
+//  Created by 박윤수 on 9/2/26.
+//
+
+import SwiftUI
+
+struct MyActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyActivityView()
+}
