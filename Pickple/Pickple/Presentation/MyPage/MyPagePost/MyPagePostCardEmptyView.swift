4e4e22961fea598bc0cdd643333d1c6c2abf012e
sooji7435 <bys0634@naver.com>
2026-09-02 20:20:08 +0900

    feat: 작성한 게시물이 없을 때 게시물 작성버튼으로 연결되는 MyPagePostCardEmptyView 생성

--- Pickple/Pickple/Presentation/MyPage/MyPagePost/MyPagePostCardEmptyView.swift
+++ Pickple/Pickple/Presentation/MyPage/MyPagePost/MyPagePostCardEmptyView.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 struct MyPagePostCardEmptyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {}) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(width: 160, height: 238) //Fixed
+                    .foregroundStyle(Color.neutral5)
+                
+                VStack {
+                    ZStack {
+                        Circle()
+                            .frame(width: 60, height: 60)
+                            .foregroundStyle(Color.white)
+                        Image("PickplePlus")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .foregroundStyle(Color.neutral30)
+                    }
+                    Text("새 투표 올리기")
+                        .pickpleTypography(.body02)
+                        .foregroundStyle(Color.neutral40)
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     MyPagePostCardEmptyView()
